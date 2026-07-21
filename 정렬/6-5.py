ffe383842811c1ae1d13707f0335a42e6647386c
starday119 <jy8294@naver.com>
2026-07-22 02:11:14 +0900

    이코테 정렬

---
+++ 정렬/6-5.py
@@ -0,0 +1,11 @@
+array = [5,7,9,0,3,1,6,2,4,8,]
+
+def quick_sort(array):
+    if len(array) <= 1:
+        return array
+    
+    pivot= array[0]
+    left = [x for x in array[1:] if x < pivot]
+    right = [x for x in array[1:] if x >= pivot]
+    
+    return quick_sort(left) + [pivot] + quick_sort(right)
